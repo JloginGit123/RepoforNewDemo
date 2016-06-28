99d70a45cb551317ef610e76cd213920d2c9beeb
Jayashree KR <jlogingit123>
2016-06-28 16:09:43 +0530

    IssueId::UST1 Reviewer::admin Status::OK Comments::comments OwnerId::1005033113 entityType::AUSRST

---
+++ JavaProjectDemo/src/java/BusinessClass.java
@@ -0,0 +1,11 @@
+package java;
+
+public class BusinessClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World test123!");
+
+	}
+
+}
